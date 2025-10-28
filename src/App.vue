ac8a9454dcf972076cdddc80ba0600c5ea0632df
mack <1103931738@qq.com>
2025-10-28 11:41:43 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
     <div class="app">
         hello
         <Games></Games>
+        <Person2></Person2>
         <Person></Person>
     </div>
 </template>
@@ -9,9 +10,10 @@
 <script>
     import Games from "./components/Games.vue"
     import Person from "./components/Person.vue"
+    import Person2 from "./components/Person2.vue"
     export default {
         name: 'App',
-        components: {Games, Person}
+        components: {Games, Person, Person2}
     }
 </script>
 
